5f8198f0d07ccf59f68226332860601b190ca45c
dionchoy <dionchoy.23@ichat.sp.edu.sg>
2024-04-22 14:27:52 +0800

    modified lab2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,17 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight/(height**2)
+    print(bmi)
+
+    if bmi < 18.5:
+        print("Under Weight")
+    elif bmi <= 25.0:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+    
+calculate_bmi(weight=57, height=1.73) 
