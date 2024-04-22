a8aed8f99865807979ff8c6de71b54d90821de3a
dionchoy <dionchoy.23@ichat.sp.edu.sg>
2024-04-22 16:24:09 +0800

    removed bmi code

--- Lab2.py
+++ Lab2.py
@@ -1,17 +1,3 @@
-def calculate_bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-
-    bmi = weight/(height**2)
-    print(bmi)
-
-    if bmi < 18.5:
-        print("Under Weight")
-    elif bmi <= 25.0:
-        print("Normal Weight")
-    else:
-        print("Over Weight")
-
 
 def display_main_menu():
     print("display_main_menu")
@@ -72,8 +58,6 @@ def calc_median_temperature(inputSorted):
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-
-    calculate_bmi(weight=57, height=1.73) 
 
     display_main_menu()
     inputList = get_user_input()
