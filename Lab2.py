fc53aeac1b359ff51da8b816dd8ab3b9a9bcdb0b
dionchoy <dionchoy.23@ichat.sp.edu.sg>
2024-04-22 16:10:39 +0800

    Added median function

--- Lab2.py
+++ Lab2.py
@@ -52,11 +52,23 @@ def find_min_max(inputTemp):
     return minMax
 
     
-def sort_temperature():
+def sort_temperature(inputTemp):
     print("sort_temperature")
+    inputTemp.sort()
+    return inputTemp
 
-def calc_median_temperature():
+
+def calc_median_temperature(inputSorted):
     print("calc_median_temperature")
+    length = len(inputSorted)
+    if length % 2 == 1:
+        median = inputSorted[int((length-1)/2)]
+    else:
+        median = inputSorted[int((length)/2)] + inputSorted[int((length)/2)-1]
+        median /= 2
+
+    return median
+
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -67,6 +79,9 @@ def main():
     inputList = get_user_input()
     print(calc_average_temperature(inputList))
     print(find_min_max(inputList))
+    sortedList = sort_temperature(inputList)
+    print(sortedList)
+    print(calc_median_temperature(sortedList))
 
 
 if __name__ == "__main__":
